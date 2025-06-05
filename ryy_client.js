9573723247cb8e79d412971288f2b2c27d328510
Conquerr's <yeniyuliana1984@gmail.com>
2025-06-05 20:17:26 +0700

    Update and rename ryy_client.js to ryy_client.json

--- ryy_client.js
+++
@@ -1,9 +0,0 @@
-"secretCode": "buyer_HL",
-  "description": "Hard Security", 
-  "lastUpdated": "20-04-2025",
-  "author": "Conquer", 
-  "status": "Online🦠",
-  "license": "MIT",
-  "version": "1.0.0"
-
-— t.me/conquerryy
